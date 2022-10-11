e39f770f0c56bd299f576c54f1f50b12404001ae
Mateus <msguerche@gmail.com>
2022-10-11 12:40:46 -0300

    update .env.example and route/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 200,
+        'message' => 'Bem vindo a api M2-Test'
+    ]);
 });
